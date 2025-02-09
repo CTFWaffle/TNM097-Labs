efdd872e2bf2e430ba4620e476f1cf73ee87153e
Alex Nannis <alex@nannis.se>
2025-02-09 18:12:44 +0100

    Lab 2 Complete

--- Lab2/meanSquare.m
+++ Lab2/meanSquare.m
@@ -4,6 +4,6 @@ function [maxRes,meanRes]=xyz2labWmse(M1_Cal,M2_Ref)
 [L2,a2,b2]=xyz2lab(M2_Ref(1,:),M2_Ref(2,:),M2_Ref(3,:));
 
 % Mean square error
-diff_REF_CAL=sqrt(abs((L-L2).^2+(a-a2).^2+(b-b2).^2));
+diff_REF_CAL=sqrt(abs((L-L2).^2+(a-a2).^2+(b-b2).^2))
 maxRes=max(diff_REF_CAL);
 meanRes=mean(diff_REF_CAL);
